62bd010234ada48006b5d929a07a0954039abe94
timofey <ydepression83@gmail.com>
2025-06-06 21:57:03 +0700

    changed the solution

--- medium/1493. Longest Subarray of 1's After Deleting One Element/1493. Longest Subarray of 1's After Deleting One Element.py
+++ medium/1493. Longest Subarray of 1's After Deleting One Element/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -6,22 +6,38 @@ class Solution:
         if len(nums) == 1:
             return 0
 
+        # left = 0
+        # res = nums[0]
+        # remind = 1 if res else 0
+
+        # for right in range(1, len(nums)):
+        #     if nums[right] == 1:
+        #         res = max(res, right - left)
+
+        #     if nums[right] == 0:
+        #         if remind:
+        #             remind = 0
+        #         else:
+        #             while not remind:
+        #                 if nums[left] == 0:
+        #                     remind = 1
+        #                 left += 1
+        #             remind = 0
+
+        # return res
+
         left = 0
-        res = nums[0]
-        remind = 1 if res else 0
+        res = 0
+        zeroes = 0
 
-        for right in range(1, len(nums)):
-            if nums[right] == 1:
-                res = max(res, right - left)
-
+        for right in range(len(nums)):
             if nums[right] == 0:
-                if remind:
-                    remind = 0
-                else:
-                    while not remind:
-                        if nums[left] == 0:
-                            remind = 1
-                        left += 1
-                    remind = 0
+                zeroes += 1
+
+            while zeroes > 1:
+                zeroes -= 1 if not nums[left] else 0
+                left += 1
+
+            res = max(res, right - left)
 
         return res
