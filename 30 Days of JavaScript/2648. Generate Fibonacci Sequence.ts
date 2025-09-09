860e26ddd55f95da796e5b43a28bba138760300d
timofey <ydepression83@gmail.com>
2025-09-09 21:32:48 +0700

    solved 'Generate Fibonacci Sequence' problem

---
+++ 30 Days of JavaScript/2648. Generate Fibonacci Sequence.ts
@@ -0,0 +1,22 @@
+function* fibGenerator(): Generator<number, any, number> {
+    let a = 0;
+    let b = 1;
+
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b]
+    }
+};
+
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
