1ca43f1a117a16c2747067eacb0204be7f449be1
timofey <ydepression83@gmail.com>
2025-09-23 13:18:03 +0700

    added solution for 'Find Minimum in Rotated Sorted Array' problem

--- medium/153. Find Minimum in Rotated Sorted Array/153. Find Minimum in Rotated Sorted Array.ts
+++ medium/153. Find Minimum in Rotated Sorted Array/153. Find Minimum in Rotated Sorted Array.ts
@@ -1,4 +1,10 @@
 function findMin(nums: number[]): number {
+    return solution2(nums)
+};
+
+function solution1(nums: number[]): number {
+    // TIME: O(N)
+
     let start = 0
 
     for (let i = 0; i < nums.length - 1; i++) {
@@ -9,4 +15,25 @@ function findMin(nums: number[]): number {
     }
 
     return nums[start]
-};
+}
+
+function solution2(nums: number[]): number {
+    // TIME: O(log N)
+
+    const n = nums.length
+    let low = 0, high = n - 1
+    let res = 0
+
+    while (low <= high) {
+        const middle = low + Math.floor((high - low) / 2)
+
+        if (nums[middle] <= nums[n - 1]) {
+            res = middle
+            high = middle - 1
+        } else {
+            low = middle + 1
+        }
+    }
+
+    return nums[res]
+}
